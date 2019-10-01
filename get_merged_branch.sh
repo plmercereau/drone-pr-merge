c17064806b58516877eef0074e05c45769ce06ee
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-01 11:17:19 +0200

    get merged origin from github api

--- get_merged_branch.sh
+++ get_merged_branch.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# echo `git branch --merged master --format "%(refname:strip=2)"`
-# current_branch=$(git branch | grep \* | cut -d ' ' -f2)
-# current_branch="$DRONE_BRANCH"
-# git branch --merged
-# git branch -r
-# for branch in $(git branch --merged --format "%(refname:strip=2)"); do
-#     if [ "$branch" != "$current_branch" ] && [ -z $(git diff "$branch" "$current_branch") ] ; then
-#         echo "$branch seems to have been merged"
-#     fi
-# done
-curl "https://api.github.com/search/issues?q=$DRONE_COMMIT&repo:$DRONE_REPO&is:merged"
+payload=$(curl -s "https://api.github.com/search/issues?q=$DRONE_COMMIT&repo:$DRONE_REPO&is:merged")
+total_count=$(echo "$payload" | jq -r '.total_count')
+if [ $total_count = 1 ]; then
+    pull_request_number=$(echo "$payload" | jq -r '.items[0].number')
+    pr_payload=$(curl -s "https://api.github.com/repos/$DRONE_REPO/pulls/$pull_request_number")
+    initial_branch=$( echo "$pr_payload" | jq -r '.head.ref')
+    echo "$initial_branch"
+else
+  echo "try again"
+fi
