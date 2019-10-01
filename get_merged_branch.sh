912a59184db27704ad97060871aa372cde81a0c0
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-01 10:40:08 +0200

    curl github api

--- get_merged_branch.sh
+++ get_merged_branch.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # echo `git branch --merged master --format "%(refname:strip=2)"`
 # current_branch=$(git branch | grep \* | cut -d ' ' -f2)
-echo "1"
-current_branch="$DRONE_BRANCH"
-echo "2"
-git branch --merged
-git branch -r
-for branch in $(git branch --merged --format "%(refname:strip=2)"); do
-echo "3"
-echo "$branch"
-    if [ "$branch" != "$current_branch" ] && [ -z $(git diff "$branch" "$current_branch") ] ; then
-        echo "$branch seems to have been merged"
-    fi
-done
+# current_branch="$DRONE_BRANCH"
+# git branch --merged
+# git branch -r
+# for branch in $(git branch --merged --format "%(refname:strip=2)"); do
+#     if [ "$branch" != "$current_branch" ] && [ -z $(git diff "$branch" "$current_branch") ] ; then
+#         echo "$branch seems to have been merged"
+#     fi
+# done
+curl "https://api.github.com/search/issues?q=is:pr&is:merged&$DRONE_REPO&SHA:$DRONE_COMMIT"
